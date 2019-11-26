3c0f3aceda57c78e7b20b27ff7ce9431e7337a04
michael.tian <michael.tian@loweprofero.com>
2019-11-26 16:46:41 +0800

    refactor story player with mplayer.

--- StoryPlayer.py
+++ StoryPlayer.py
@@ -2,51 +2,128 @@
 import os
 import re
 import json
+import time
+import mplayer
 from robot import utils, config, logging, constants
-from robot.Player import MusicPlayer
+from robot.Player import AbstractPlayer
 from robot.sdk.AbstractPlugin import AbstractPlugin
 
 logger = logging.getLogger(__name__)
 
+class MPlayer(AbstractPlayer):
+
+    SLUG = 'MPlayer'
+
+    def __init__(self, **kwargs):
+        super(MPlayer, self).__init__(**kwargs)
+
+        self.playing = False
+        self.player = mplayer.Player()
+        self.onCompleteds = []
+
+    def play(self, src, time_pos, onCompleted=None):
+        if os.path.exists(src) or src.startswith('http'):
+            if onCompleted is not None:
+                self.onCompleteds.append(onCompleted)
+
+            if not self.player.is_alive():
+                self.player = mplayer.Player()
+
+            self.player.loadfile(src)
+            self.player.time_pos = time_pos
+            self.playing = True
+            while True:
+                if not self.player.length:
+                    break
+                time.sleep(1)
+            self.playing = False
+
+            for onCompleted in self.onCompleteds:
+                if onCompleted is not None:
+                    onCompleted()
+        else:
+            logging.critical(f'file path not exists: {src}')
+
+    def appendOnCompleted(self, onCompleted):
+        if onCompleted is not None:
+            self.onCompleteds.append(onCompleted)
+
+    def stop(self):
+        #if self.player.is_alive():
+        #    self.player.quit()
+        self.pause()
+
+    def quit(self):
+        if self.player.is_alive():
+            self.player.quit()
+
+    def pause(self):
+        if not self.player.paused:
+            self.player.pause()
+
+    def resume(self):
+        if self.player.paused:
+            self.player.pause()
+
+    def is_playing(self):
+        return self.playing
+
+    def is_pausing(self):
+        return self.player.paused
+
+    @property
+    def time_pos(self):
+        return self.player.time_pos
+
+    @property
+    def speed(self):
+        return self.player.speed
+
+    @property
+    def volume(self):
+        return self.player.volume
+
 """
 给故事播放器插件使用的，
-在 MusicPlayer 的基础上添加了播放状态保存与读取。
+在 MPlayer 的基础上添加了播放列表，状态保存与读取。
 """
-class StoryPlayer(MusicPlayer):
+class StoryPlayer(MPlayer):
+
     SLUG = 'StoryPlayer'
 
     def __init__(self, playlist, plugin, **kwargs):
-        super(StoryPlayer, self).__init__(playlist, plugin, **kwargs)
+        super(StoryPlayer, self).__init__(**kwargs)
         self.album = None
-        self.playlist = None
-        self.idx = None
+        self.playlist = playlist
+        self.plugin = plugin
+        self.idx = 0
         self.status_path = os.path.join(constants.DATA_PATH, 'story')
 
-    def play(self):
+    def play(self, time_pos=0):
         logger.debug('StoryPlayer play')
         path = self.playlist[self.idx]
         logger.info(f'目前正在播放：{self.playlist[self.idx]}')
         self.plugin.say(f'{self.get_song_name()}', cache=True, wait=True)
-        super(MusicPlayer, self).stop()
-        super(MusicPlayer, self).play(path, False, self.next)
+        super(MPlayer, self).stop()
+        super(MPlayer, self).play(path, time_pos, self.next)
         # save play status
         self.save_playstatus()
 
     def next(self):
         logger.debug('StoryPlayer next')
-        super(MusicPlayer, self).stop()
+        super(MPlayer, self).stop()
         self.idx = (self.idx+1) % len(self.playlist)
         self.play()
     
     def prev(self):
         logger.debug('StoryPlayer prev')
-        super(MusicPlayer, self).stop()
+        super(MPlayer, self).stop()
         self.idx = (self.idx-1) % len(self.playlist)
         self.play()
 
     def first(self):
         logger.debug('StoryPlayer play first')
-        super(MusicPlayer, self).stop()
+        super(MPlayer, self).stop()
         self.idx = 0
         self.play()
 
@@ -57,43 +134,64 @@ class StoryPlayer(MusicPlayer):
     def pause(self):
         logger.debug('StoryPlayer pause')
         super().pause()
-        #self.save_playstatus()
+        self.save_playstatus()
     
     def stop(self):
         logger.debug('StoryPlayer stop')
         super().stop()
-        #self.save_playstatus()
+        self.save_playstatus()
     
     def update_playlist(self, album, playlist):
-        super(MusicPlayer, self).stop()
+        super(MPlayer, self).stop()
         self.album = album
         self.playlist = playlist
-        self.idx = self.get_playstatus()
+        time_pos = 0
+        self.status = self.get_playstatus()
         # check if continue
-        if self.idx:
+        if self.status and (self.status['idx'] or self.status['time_pos']):
+            time_pos= self.status['time_pos']
             self.plugin.say(f'继续播放{album}', cache=True, wait=True)
         else:
             self.plugin.say(f'马上为您播放{album}', cache=True, wait=True)
-        self.play()
+
+        self.play(time_pos)
     
     def get_playstatus(self):
         tmp_status_path = os.path.join(self.status_path, f'{self.album}.json')
         if not os.path.exists(tmp_status_path):
-            return 0
-        return json.loads(utils.get_file_content(tmp_status_path))['idx']
+            return None
+        return json.loads(utils.get_file_content(tmp_status_path))
 
     def save_playstatus(self):
         tmp_status_path = os.path.join(self.status_path, f'{self.album}.json')
         if os.path.exists(tmp_status_path):
             os.remove(tmp_status_path)
         with open(tmp_status_path, 'w+', encoding='utf-8') as f:
-            f.write(json.dumps({'idx': self.idx}))
+            f.write(json.dumps({'idx': self.idx, 'time_pos': self.time_pos if self.time_pos else 0}))
     
     def get_song_name(self):
         path = self.playlist[self.idx]
         name = path.split('/')[-1]
         name = re.sub(r"^\d*\.*","", name)
         return re.sub(r"\.mp3|\.m4a|\.wav", "", name)
+
+    def turn_up(self):
+        volume = self.volume
+        volume += 10
+        if volume >= 100:
+            volume = 100
+            self.plugin.say('音量已经最大啦', wait=True)
+        self.volume = volume
+        self.resume()
+
+    def turn_down(self):
+        volume = self.volume
+        volume -= 10
+        if volume <= 10:
+            volume = 10
+            self.plugin.say('音量已经最小啦', wait=True)
+        self.volume = volume
+        self.resume()
 
 class Plugin(AbstractPlugin):
 
@@ -145,16 +243,16 @@ class Plugin(AbstractPlugin):
                 if slot['name'] == 'user_d':
                     word = self.nlu.getSlotWords(parsed, 'CHANGE_VOL', 'user_d')[0]
                     if word == '--HIGHER--':
-                        self.player.turnUp()
+                        self.player.turn_up()
                     else:
-                        self.player.turnDown()
+                        self.player.turn_down()
                     return
                 elif slot['name'] == 'user_vd':
                     word = self.nlu.getSlotWords(parsed, 'CHANGE_VOL', 'user_vd')[0]
                     if word == '--LOUDER--':
-                        self.player.turnUp()
+                        self.player.turn_up()
                     else:
-                        self.player.turnDown()
+                        self.player.turn_down()
 
         elif self.nlu.hasIntent(parsed, 'PAUSE'):
             self.player.pause()
@@ -176,7 +274,7 @@ class Plugin(AbstractPlugin):
             self.player.resume()
 
     def isValidImmersive(self, text, parsed):
-        return any(self.nlu.hasIntent(parsed, intent) for intent in ['CHANGE_TO_LAST', 'CHANGE_TO_NEXT', 'CHANGE_VOL', 'CLOSE_MUSIC', 'PAUSE', 'CONTINUE'])
+        return any(self.nlu.hasIntent(parsed, intent) for intent in ['CHANGE_TO_LAST', 'CHANGE_TO_NEXT', 'RESTART_MUSIC', 'CHANGE_VOL', 'CLOSE_MUSIC', 'PAUSE', 'CONTINUE'])
 
     def isValid(self, text, parsed):
         return "播放" in text
